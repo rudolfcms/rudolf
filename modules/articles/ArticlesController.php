ba3138ebe73817e9cbd70aabb6ad1254dce90b98
Mikołaj Pich <m.pich@outlook.com>
2015-10-30 08:30:21 +0000

    Update methods (not tested)

--- modules/articles/ArticlesController.php
+++ modules/articles/ArticlesController.php
@@ -1,14 +1,47 @@
 <?php
-
+/**
+ * This file is part of lcms articles module.
+ * 
+ * This is the controller of articles module.
+ * 
+ * @author Mikołaj Pich <m.pich@outlook.com>
+ * @package lcms
+ * @version 0.1
+ */
+ 
 namespace Modules\articles;
-use Modules\articles;
+use lcms\Abstracts\Controller;
 
-class ArticlesController {
-	public function index($page) {
-		echo $page;
+class ArticlesController extends Controller {
+	
+	/**
+	* Manage model and view to display articles list
+	*
+	* @param int $page Page number
+	*
+	* @return bool|string
+	*/
+	public function articlesList($page) {
+	    $model = new ArticlesListModel();
+	    $view = new ArticlesListView();
+	    
+	    $results = $model->getArticlesListArray($page, );
+		if(false === $results) {
+			return false;
+		}
+		
+	    return $view->getArticlesListaPage($results);
 	}
 
-	public function one($a, $b, $c) {
-		echo $a . $b . $c;
+	public function one($year, $month, $slug) {
+		$model = new ArticlesOnceModel();
+		$view = new ArticlesOnceView();
+		
+		$results = $model->getArticleOnceArray($year, $month, $slug);
+		if(false === $results) {
+			return false;
+		}
+		
+		return $view->getArticleOncePage($results);
 	}
 }
