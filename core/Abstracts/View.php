eee2df06ba7e53d90f9735ddb917efb7b129614d
Mikołaj Pich <m.pich@outlook.com>
2015-11-14 00:16:24 +0100

    Add comments, rename $path to $themePath, add loadconfig() method

--- core/Abstracts/View.php
+++ core/Abstracts/View.php
@@ -14,15 +14,34 @@ namespace lcms\Abstracts;
 
 abstract class View {
 
-	public $path;
+	/**
+	 * @var string Server-side path to theme catalog
+	 */
+	public $themePath;
 
+	/**
+	 * @var object
+	 */
+	public $theme;
+
+	/**
+	 * Render page
+	 * 
+	 * @param string $side front|admin
+	 * @param string $type html|json
+	 * 
+	 * @return void
+	 */
 	public function render($side = 'front', $type = 'html') {
 		if('front' !== $side or 'admin' !== $side) {
 			$this->side = 'front';
-			$this->path = LTHEMES . '/front/' . FRONT_THEME;
 		} else {
 			$this->side = $side;
 		}
+		
+		$this->themePath = LTHEMES .'/front/'. FRONT_THEME;
+
+		$this->loadConfig();
 
 		switch ($type) {
 			case 'json':
@@ -35,8 +54,13 @@ abstract class View {
 		}
 	}
 
+	/**
+	 * Render page in html
+	 * 
+	 * @return void
+	 */
 	private function renderHtml() {
-		$file = LTHEMES_ROOT . '/'. $this->side .'/'. FRONT_THEME . '/templates/' . $this->template .'.html.php';
+		$file = LTHEMES_ROOT .'/'. $this->side .'/'. FRONT_THEME .'/templates/'. $this->template .'.html.php';
 		
 		try {
 			if(is_file($file)) {
@@ -49,8 +73,26 @@ abstract class View {
 		}
 	}
 
+	/**
+	 * Render page in json
+	 * 
+	 * @return void
+	 */
 	private function renderJson() {
 		header('Content-Type: application/json');
 		echo json_encode($this->data);
+	}
+	
+	/**
+	 * Load theme config class
+	 */
+	private function loadConfig() {
+		$file = LTHEMES_ROOT .'/front/'. FRONT_THEME .'/'. FRONT_THEME .'.php';
+		
+		if(is_file($file)) {
+			include $file;
+			$class = ucfirst(FRONT_THEME);
+			$this->theme = new $class();
+		}
 	}
 }
