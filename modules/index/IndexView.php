3a90417b1f8d233d62e75c61583c54d2d9556997
Mikołaj Pich <m.pich@outlook.com>
2015-11-14 14:11:12 +0100

    Add $paginationInfo param to setData()

--- modules/index/IndexView.php
+++ modules/index/IndexView.php
@@ -5,8 +5,10 @@ use lcms\Abstracts\View;
 
 class IndexView extends \Modules\articles\ArticlesListView {
 	
-	public function setData($data) {
+	public function setData($data, $paginationInfo) {
 		$this->data = $data;
+
+		$this->paginationInfo = $paginationInfo;
 
 		$this->template = 'index';
 	}
