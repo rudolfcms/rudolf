b11fff26eb7670235268629c14da931fe904986b
Mikołaj Pich <m.pich@outlook.com>
2015-11-14 01:41:54 +0100

    Add try {} block

--- modules/articles/ArticleOneModel.php
+++ modules/articles/ArticleOneModel.php
@@ -17,38 +17,42 @@ use lcms\Abstracts\Model,
 class ArticleOneModel extends Model {
 
 	public function getOneByDate($year, $month, $slug) {
-		$stmt = $this->pdo->prepare("SELECT 
-				-- article fields
-				a.id, a.category_id, a.title, a.keywords, a.description, a.content, a.author, 
-				a.added_by, a.date, a.added, a.modified, a.modified_by, a.views, 
-				a.slug, a.album, a.thumb, a.photos, a.published, 
+		try {	
+			$stmt = $this->pdo->prepare("SELECT 
+					-- article fields
+					a.id, a.category_id, a.title, a.keywords, a.description, a.content, a.author, 
+					a.added_by, a.date, a.added, a.modified, a.modified_by, a.views, 
+					a.slug, a.album, a.thumb, a.photos, a.published, 
 
-				-- user fields
-				u.first_name, u.surname, 
+					-- user fields
+					u.first_name, u.surname, 
 
-				m.first_name as 'modified_first_name', m.surname as 'modified_surname', 
+					m.first_name as 'modified_first_name', m.surname as 'modified_surname', 
 
-				-- category fields
-				c.title as category_title, c.slug as category_url 
+					-- category fields
+					c.title as category_title, c.slug as category_url 
 
-				FROM {$this->prefix}articles as a 
+					FROM {$this->prefix}articles as a 
 
-				-- user join on added_by id
-				LEFT JOIN {$this->prefix}users as u ON a.added_by=u.id 
+					-- user join on added_by id
+					LEFT JOIN {$this->prefix}users as u ON a.added_by=u.id 
 
-				-- user join on modified_by id
-				LEFT JOIN {$this->prefix}users as m ON a.modified_by=m.id
+					-- user join on modified_by id
+					LEFT JOIN {$this->prefix}users as m ON a.modified_by=m.id
 
-				-- category join on article category_id
-				LEFT JOIN {$this->prefix}categories as c ON a.category_id=c.id 
+					-- category join on article category_id
+					LEFT JOIN {$this->prefix}categories as c ON a.category_id=c.id 
 
-				WHERE YEAR(`date`) = :year AND MONTH(`date`) = :month AND a.slug = :slug
-			");
-		$stmt->bindValue(':year', $year, PDO::PARAM_INT);
-		$stmt->bindValue(':month', $month, PDO::PARAM_INT);
-		$stmt->bindValue(':slug', $slug, PDO::PARAM_STR);
-		$stmt->execute();
-		$results = $stmt->fetch(PDO::FETCH_ASSOC);
+					WHERE YEAR(`date`) = :year AND MONTH(`date`) = :month AND a.slug = :slug
+				");
+			$stmt->bindValue(':year', $year, PDO::PARAM_INT);
+			$stmt->bindValue(':month', $month, PDO::PARAM_INT);
+			$stmt->bindValue(':slug', $slug, PDO::PARAM_STR);
+			$stmt->execute();
+			$results = $stmt->fetch(PDO::FETCH_ASSOC);
+		} catch(\PDOException $e) {
+			die($e);
+		}
 		
 		if(empty($results)) {
 			return false;
