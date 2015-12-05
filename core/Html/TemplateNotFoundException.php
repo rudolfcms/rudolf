127ac05acfa56ac33143870caece2f6f4cc43043
Mikołaj Pich <m.pich@outlook.com>
2015-12-05 22:56:44 +0100

    Add Exceptions

---
+++ core/Html/TemplateNotFoundException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This file is part of Rudolf.
+ *
+ * Template Not Found Exception.
+ *
+ * @author Mikołaj Pich <m.pich@outlook.com>
+ * @package Rudolf\Html
+ * @version 0.1
+ */
+
+namespace Rudolf\Html;
+
+class TemplateNotFoundException extends \Exception { }
---
+++ core/Html/ThemeNotFoundException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This file is part of Rudolf.
+ *
+ * Theme Not Found Exception.
+ *
+ * @author Mikołaj Pich <m.pich@outlook.com>
+ * @package Rudolf\Html
+ * @version 0.1
+ */
+
+namespace Rudolf\Html;
+
+class ThemeNotFoundException extends \Exception { }
