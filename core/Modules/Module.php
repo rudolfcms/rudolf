948f253aba3ffb694cffbfae1ff8bad37ff1c700
Mikołaj Pich <m.pich@outlook.com>
2016-01-09 17:31:17 +0100

    Add Module class body, getConfig()

--- core/Modules/Module.php
+++ core/Modules/Module.php
@@ -12,5 +12,21 @@
 namespace Rudolf\Modules;
 
 class Module {
+	public function __construct($name) {
+		$this->name = $name;
+		
+		if(empty($name)) {
+			throw new \InvalidArgumentException("Invalid module name");
+		}
+	}
 
+	public function getConfig() {
+		$file = CONFIG_ROOT . DIRECTORY_SEPARATOR . 'modules' . DIRECTORY_SEPARATOR . $this->name . '.php';
+
+		if(!file_exists($file)) {
+			throw new \Exception("{$this->name} module configuration does not exist");
+
+		}
+		return include $file;
+	}
 }
