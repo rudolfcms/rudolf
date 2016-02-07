ebdf4aeab84e28307d10eb120d2a0cc80adbe7c9
Mikołaj Pich <m.pich@outlook.com>
2016-02-07 19:30:59 +0100

    Add methods for create breadcrumbs and page navigation

--- core/Html/Navigation.php
+++ core/Html/Navigation.php
@@ -12,7 +12,72 @@
 namespace Rudolf\Html;
 
 class Navigation {
-	
+
+	/**
+	 * It create page navigation
+	 * 
+	 * @param array $items Array of navigation items
+	 * @param int $current Id of current page
+	 * @param array $classes
+	 * @param int $nesting
+	 * 
+	 * @return string
+	 */
+	public function createPageNavigation($type, $items, $current, $classes, $nesting = 0) {
+		$builder = new \Rudolf\Libs\MenuBuilder();
+		foreach ($items as $key => $value) {
+			if($type === $value['menu_type']) {
+				$newItems[] = $items[$key];
+			}
+		}
+
+		return $builder->getMenuHtml(0, $newItems, DIR.'/', $classes, $current, $nesting);
+	}
+
+	/**
+	 * It create breadcrumb
+	 *
+	 * @param boolean $a create breadcrumb or only return array with breadrumb data
+	 * @param array $menu array with menu elements
+	 * @param array $address Address array
+	 * @param int $nesting
+	 *
+	 * @return array|string
+	 */
+	public function createBreadcrumbsNavigation($a, $menu, $address, $nesting = 0) {
+		$url = null;
+
+		for($pid = 0, $i = 0; $i < count($address); $i++) {
+			if($pid == $menu[$address[$i]]['parent_id']) {
+				$array[$i] = array($url . '/' . $address[$i], $menu[$address[$i]]['title']);
+				$pid = $menu[$address[$i]]['id'];
+				$url .= '/' . $address[$i];
+			}
+		}
+
+		if($a) {
+			return $array;
+		} elseif(!$a) {
+			$html[] = '<li><a href="' . DIR . '">Start</a></li>';
+
+			$tab = str_repeat("\t", $nesting);
+
+			for($i = 0; $i < (count($array) - 1); $i++) {
+				$html[] = sprintf('%1$s<li><a href="%2$s">%3$s</a></li>',
+					$tab, // nesting
+					DIR . $array[$i][0],
+					$array[$i][1]
+				);
+			}
+
+			$html[] = sprintf('%1$s<li class="active">%2$s</li>',
+				$tab, // nesting
+				$array[$i][1]
+			);
+			
+			return implode("\r\n", $html);
+		}
+	}
 
 	/**
 	 * It created paging navigation
@@ -27,6 +92,7 @@ class Navigation {
 	 * 
 	 * @param string $path path with a slash at the beginning and at the end without him, like: '/kg'
 	 * @param array $classes specifies a pagination appearance
+	 * @param int $nesting
 	 * 
 	 * @return string
 	 */
