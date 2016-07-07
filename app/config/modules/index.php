366173538c5a3ae933462ea2fb420f38da433cb5
Mikołaj Pich <m.pich@outlook.com>
2016-07-07 20:15:19 +0200

    Delete modules config

--- app/config/modules/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return array(
-	'on_page' => 10,
-	'nav_number' => 7,
-	'order' => 'DESC',
-	'sort' => 'date'
-);
--- app/config/modules/koxy.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array(
-	'on_page' => 8,
-	'nav_number' => 7,
-	'order' => 'DESC',
-	'sort' => 'id',
-	'priority' => 2000
-);
