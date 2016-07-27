21a83bde52050b21479ec215346daa7d743f6989
Mikołaj Pich <m.pich@outlook.com>
2016-07-27 19:34:26 +0200

    Get more data in categories list

--- app/module/Categories/Roll/Admin/Model.php
+++ app/module/Categories/Roll/Admin/Model.php
@@ -15,9 +15,21 @@ class Model extends Roll\Model
             SELECT category.id,
                    category.title,
                    category.slug,
+                   category.added,
+                   category.modified,
+                   adder.nick AS adder_nick,
+                   adder.first_name AS adder_first_name,
+                   adder.surname AS adder_surname,
+                   adder.email AS adder_email,
+                   modifier.nick AS adder_nick,
+                   modifier.first_name AS modifier_first_name,
+                   modifier.surname AS modifier_surname,
+                   modifier.email AS modifier_email,
                    COUNT(items.id) AS total
             FROM {$this->prefix}categories AS category
             LEFT JOIN {$this->prefix}$type AS items ON category.id=items.category_ID
+            LEFT JOIN {$this->prefix}users AS adder ON category.adder_ID = adder.id
+            LEFT JOIN {$this->prefix}users AS modifier ON category.modifier_ID = modifier.id
             WHERE category.type = '$type'
             GROUP BY category.id
             ORDER BY $orderBy[0] $orderBy[1] LIMIT $limit,
