53a40406e150b9b0f964a81de89f6d580f8f5a24
Mikołaj Pich <m.pich@outlook.com>
2016-08-17 19:59:25 +0200

    Update modules config

--- app/install/config.example/modules.php
+++ app/install/config.example/modules.php
@@ -1,12 +1,11 @@
-<?php
-
-return [
+<?php return array (
   'Albums' => 1,
   'Articles' => 1,
   'Dashboard' => 1,
+  'Galleries' => 1,
   'Index' => 1,
-  'Koxy' => 1,
+  'Koxy' => 0,
+  'Modules' => 1,
   'Pages' => 1,
   'Users' => 1,
-  'Galleries' => 1,
-];
+);
