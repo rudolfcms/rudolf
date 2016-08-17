97e074ee6f907c00d4629adda7c854e57e3358fe
Mikołaj Pich <m.pich@outlook.com>
2016-08-17 21:05:47 +0200

    Delete useless spaces

--- app/defines.php
+++ app/defines.php
@@ -18,18 +18,18 @@ if (dirname($_SERVER['SCRIPT_NAME']) == DIRECTORY_SEPARATOR) {
     define('DIR', dirname($_SERVER['SCRIPT_NAME']));
 }
 
-define('PLUGINS',               DIR.'/content/plugins');
-define('THEMES',                DIR.'/content/themes');
-define('UPLOADS',               DIR.'/content/uploads');
-define('CONTENT',               DIR.'/content');
+define('PLUGINS',       DIR.'/content/plugins');
+define('THEMES',        DIR.'/content/themes');
+define('UPLOADS',       DIR.'/content/uploads');
+define('CONTENT',       DIR.'/content');
 
-define('PLUGINS_ROOT',          WEB_ROOT.'/content/plugins');
-define('THEMES_ROOT',           WEB_ROOT.'/content/themes');
-define('UPLOADS_ROOT',          WEB_ROOT.'/content/uploads');
+define('PLUGINS_ROOT',  WEB_ROOT.'/content/plugins');
+define('THEMES_ROOT',   WEB_ROOT.'/content/themes');
+define('UPLOADS_ROOT',  WEB_ROOT.'/content/uploads');
 
-define('APP_ROOT',              __DIR__);
-define('CONFIG_ROOT',           APP_ROOT.'/config');
-define('CORE_ROOT',             APP_ROOT.'/core');
-define('TEMP_ROOT',             APP_ROOT.'/temp');
-define('MODULES_ROOT',          APP_ROOT.'/module');
-define('LOG_ROOT',              APP_ROOT.'/log');
+define('APP_ROOT',      __DIR__);
+define('CONFIG_ROOT',   APP_ROOT.'/config');
+define('CORE_ROOT',     APP_ROOT.'/core');
+define('TEMP_ROOT',     APP_ROOT.'/temp');
+define('MODULES_ROOT',  APP_ROOT.'/module');
+define('LOG_ROOT',      APP_ROOT.'/log');
