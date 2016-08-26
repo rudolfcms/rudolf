1460a3f2d6e39c4081b0430e97de07f98e1df140
Mikołaj Pich <m.pich@outlook.com>
2016-08-27 01:01:37 +0200

    Add phpixie/image and use it in Images\Resizer

--- app/component/Images/Resizer.php
+++ app/component/Images/Resizer.php
@@ -2,10 +2,12 @@
 
 namespace Rudolf\Component\Images;
 
+use PHPixie\Image as ImageManipulator;
+
 class Resizer
 {
     private $type = 'internal';
-    private $cacheExtension = '.cache';
+    private $cacheExtension = '.jpg';
     private $cacheDirectory = TEMP_ROOT.'/imageresizer/';
     private $allowedTypes = ['gif', 'png', 'jpeg'];
 
@@ -41,7 +43,6 @@ class Resizer
         } else {
             $this->serveInternal();
         }
-        // print_r($this->src);
     }
 
     /**
@@ -97,15 +98,14 @@ class Resizer
      */
     private function serveExternal()
     {
-        $temp = $this->fetchFromRemote($this->src);
-    }
+        $cacheFile = $this->createCacheName();
 
-    /**
-     * @return string Temp filename
-     */
-    private function fetchFromRemote($src)
-    {
-        return;
+        $manipulator = new ImageManipulator();
+        $image = $manipulator->load(file_get_contents($this->src));
+        $image->fill($this->width, $this->height, true);
+        $image->save($cacheFile);
+
+        $this->serveFromCache($cacheFile);
     }
 
     /**
@@ -121,8 +121,11 @@ class Resizer
 
         $cacheFile = $this->createCacheName();
 
-        // $this->resize($file, $cacheFile, $this->width, $this->height);
-        $this->resizeAndCrop($file, $cacheFile, $this->width, $this->height);
+        $manipulator = new ImageManipulator();
+        $image = $manipulator->read($file);
+        $image->fill($this->width, $this->height, true);
+        $image->save($cacheFile);
+
         $this->serveFromCache($cacheFile);
     }
 
@@ -168,140 +171,5 @@ class Resizer
         }
 
         return $this->src;
-    }
-
-    /**
-     * Resize image and crop.
-     *
-     * @param string $file   Source image
-     * @param string $target Target image filename
-     * @param int    $width  New image width
-     * @param int    $height New image Height
-     *
-     * @return bool
-     */
-    public function resizeAndCrop($file, $target, $width, $height)
-    {
-        $mimetype = $this->getImageType($file);
-        list($newWidth, $newHeight) = getimagesize($file);
-
-        // Loop through until we get the dimensions we want.
-        $n = 0;
-        while ($newWidth > $width && $newHeight > $height) {
-            echo $width;
-            $newWidth /= 1.002;
-            $newHeight /= 1.002;
-            ++$n;
-        }
-        $newWidth = floor($newWidth);
-        $newHeight = floor($newHeight);
-
-        // Create the temporary file
-        $tempFile = tempnam(sys_get_temp_dir(), 'resizer');
-
-        $this->resize($file, $tempFile, $newWidth, $newHeight);
-
-        // Create images to crop
-        $canvas = imagecreatetruecolor($width, $height);
-
-        // for png
-        imagealphablending($canvas, false);
-        imagesavealpha($canvas, true);
-
-        switch ($mimetype) {
-            case 'gif':
-                $image = imagecreatefromgif($tempFile);
-                break;
-            case 'png':
-                $image = imagecreatefrompng($tempFile);
-                break;
-            case 'jpeg':
-            default:
-                $image = imagecreatefromjpeg($tempFile);
-                break;
-        }
-
-        imagecopyresampled($canvas, $image, 0, 0, 0, 0, $width, $height, $width, $height);
-
-        // What image are we creating?
-        switch ($mimetype) {
-            case 'gif':
-                imagegif($canvas, $target, 100);
-                break;
-            case 'png':
-                imagepng($canvas, $target, 9);
-                break;
-            case 'jpeg':
-            default:
-                imagejpeg($canvas, $target, 100);
-                break;
-        }
-        imagedestroy($canvas);
-        imagedestroy($image);
-
-        // Delete the temporary file
-        unlink($tempFile);
-    }
-
-    /**
-     * Resize image.
-     *
-     * @param string $file   Source image
-     * @param string $target Target image filename
-     * @param int    $width  New image width
-     * @param int    $height New image Height
-     *
-     * @return bool
-     */
-    public function resize($file, $target, $width, $height)
-    {
-        $type = $this->getImageType($file);
-
-        switch ($type) {
-            case 'gif':
-                $src = imagecreatefromgif($file);
-                break;
-            case 'png':
-                $src = imagecreatefrompng($file);
-                break;
-            case 'jpeg':
-            default:
-                $src = imagecreatefromjpeg($file);
-                break;
-        }
-
-        list($oldWidth, $oldHeight) = getimagesize($file);
-        $tmp = imagecreatetruecolor($width, $height);
-
-        // for png
-        imagealphablending($tmp, false);
-        imagesavealpha($tmp, true);
-
-        imagecopyresampled($tmp, $src, 0, 0, 0, 0, $width, $height, $oldWidth, $oldHeight);
-
-        switch ($type) {
-            case 'jpg':
-                imagejpeg($tmp, $target, 100);
-                break;
-            case 'jpeg':
-                imagejpeg($tmp, $target, 100);
-                break;
-            case 'gif':
-                imagegif($tmp, $target, 100);
-                break;
-            case 'png':
-                imagepng($tmp, $target, 9);
-                break;
-            case 'wbm':
-                imagewbmp($tmp, $target, 100);
-                break;
-            default:
-                imagejpeg($tmp, $target, 100);
-                break;
-        }
-        imagedestroy($src);
-        imagedestroy($tmp);
-
-        return true;
     }
 }
