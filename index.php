3c50e97a7c25eb7d3ebd10786703772fa826b756
Mikołaj Pich <m.pich@outlook.com>
2016-09-02 01:29:20 +0200

    Delete comment

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
 <?php
-/**
- * This file is part of Rudolf.
- *
- * This is the main web entry point for Rudolf.
- *
- * @author Mikołaj Pich <m.pich@outlook.com>
- *
- * @version 0.1
- */
+
 define('WEB_ROOT', __DIR__);
 
 require __DIR__.'/app/init.php';
