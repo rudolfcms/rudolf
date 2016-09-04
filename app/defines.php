7368838be8ed54aea37e1ddd41c936a1a967f832
Mikołaj Pich <m.pich@outlook.com>
2016-09-04 20:25:57 +0200

    Refactor defines

--- app/defines.php
+++ app/defines.php
@@ -1,32 +1,8 @@
 <?php
-/**
- * This file is part of Rudolf.
- * 
- * Define application constants.
- * 
- * @author Mikołaj Pich <m.pich@outlook.com>
- *
- * @version 0.1
- */
+
 define('VER', 0.1);
 define('VER_NAME', '0.0.10-prealpha');
 define('NAME', 'rudolf');
-
-if (dirname($_SERVER['SCRIPT_NAME']) == DIRECTORY_SEPARATOR) {
-    define('DIR', '');
-} else {
-    define('DIR', dirname($_SERVER['SCRIPT_NAME']));
-}
-
-define('PLUGINS',       DIR.'/content/plugins');
-define('THEMES',        DIR.'/content/themes');
-define('UPLOADS',       DIR.'/content/uploads');
-define('CONTENT',       DIR.'/content');
-
-define('CONTENT_ROOT',  WEB_ROOT.'/content');
-define('PLUGINS_ROOT',  WEB_ROOT.'/content/plugins');
-define('THEMES_ROOT',   WEB_ROOT.'/content/themes');
-define('UPLOADS_ROOT',  WEB_ROOT.'/content/uploads');
 
 define('APP_ROOT',      __DIR__);
 define('CONFIG_ROOT',   APP_ROOT.'/config');
@@ -34,3 +10,19 @@ define('CORE_ROOT',     APP_ROOT.'/core');
 define('TEMP_ROOT',     APP_ROOT.'/temp');
 define('MODULES_ROOT',  APP_ROOT.'/module');
 define('LOG_ROOT',      APP_ROOT.'/log');
+
+define('CONTENT_ROOT',  WEB_ROOT.'/content');
+define('PLUGINS_ROOT',  CONTENT_ROOT.'/plugins');
+define('THEMES_ROOT',   CONTENT_ROOT.'/themes');
+define('UPLOADS_ROOT',  CONTENT_ROOT.'/uploads');
+
+if (dirname($_SERVER['SCRIPT_NAME']) == '/') {
+    define('DIR', '');
+} else {
+    define('DIR', dirname($_SERVER['SCRIPT_NAME']));
+}
+
+define('CONTENT',       DIR.'/content');
+define('PLUGINS',       CONTENT.'/plugins');
+define('THEMES',        CONTENT.'/themes');
+define('UPLOADS',       CONTENT.'/uploads');
