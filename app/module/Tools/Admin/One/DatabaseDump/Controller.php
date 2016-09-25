20c48df06476b980de9409ffd87d488adb3b22e2
Mikołaj Pich <m.pich@outlook.com>
2016-09-25 15:54:19 +0200

    Change database dump lib to dg/mysql-dump
    add compress dump feature

--- app/module/Tools/Admin/One/DatabaseDump/Controller.php
+++ app/module/Tools/Admin/One/DatabaseDump/Controller.php
@@ -2,7 +2,8 @@
 
 namespace Rudolf\Modules\Tools\Admin\One\DatabaseDump;
 
-use Ifsnop\Mysqldump\Mysqldump;
+use MySQLDump;
+use mysqli;
 use Rudolf\Component\Http\Response;
 use Rudolf\Framework\Controller\AdminController;
 
@@ -10,38 +11,46 @@ class Controller extends AdminController
 {
     public function show()
     {
-        if (!empty($_POST)) {
-            $opts = [
-                'default-character-set' => Mysqldump::UTF8MB4,
-            ];
-
-            if (isset($_POST['full'])) {
-                $opts['extended-insert'] = false; // item per line
-            }
-
-            if (isset($_POST['data'])) {
-                $opts['no-create-info'] = true;
-                $opts['skip-comments'] = true;
-                $opts['exclude-tables'] = ['rudolf_users_sessions'];
-            }
-
-            $filename = 'rudolf_dump_'.date('Y-m-d_H-i-s').'.sql';
+        if (isset($_POST['backup'])) {
             $dbconf = include CONFIG_ROOT.'/database.php';
-            $dns = $dbconf['engine']
-                .':dbname='.$dbconf['database']
-                .';charset='.$dbconf['charset']
-                .';host='.$dbconf['host'];
+            $dump = new MySQLDump(new mysqli(
+                $dbconf['host'],
+                $dbconf['user'],
+                $dbconf['pass'],
+                $dbconf['database'])
+            );
+            $filename = 'rudolf_'.$dbconf['database'].'_'.date('Y-m-d_H-i-s').'.sql';
 
-            $dump = new Mysqldump($dns, $dbconf['user'], $dbconf['pass'], $opts);
-            $dump->start();
+            set_time_limit(0);
+            ignore_user_abort(TRUE);
 
-            $response = new Response($source = ob_get_contents());
-            $response->setHeader(['Content-Type', 'application/octet-stream']);
-            $response->setHeader(['Content-Disposition', 'attachment; filename="'.$filename.'"']);
-            $response->setHeader(['Content-Length', strlen($source)]);
-            $response->send();
+            ob_start();
+            $dump->write();
+            $source = ob_get_clean();
+
+            $response = new Response();
+
+            if (isset($_POST['gziped'])) {
+                ini_set('zlib.output_compression', TRUE);
+                $output = gzencode($source, 9);
+                $response->setHeader(['Content-Encoding', 'gzip']);
+                $response->setHeader(['Content-Type', 'application/x-gzip']);
+                $response->setHeader(['Content-Disposition', 'attachment; filename="'.$filename.'.gz"']);
+            } else {
+                $output = $source;
+                $response->setHeader(['Content-Disposition', 'attachment; filename="'.$filename.'"']);
+                $response->setHeader(['Content-Type', 'application/sql']);
+            }
+            $response->setContent($output);
+            $response->setHeader(['Connection', 'close']);
+            $response->setHeader(['Content-Length', strlen($output)]);
+            $response->setHeader(['Expires', gmdate('D, d M Y H:i:s') . ' GMT']);
+            $response->setHeader(['Cache-Control', 'no-cache']);
+
+            echo $response->send();
             exit;
         }
+
         $view = new View();
         $view->setData($data = []);
         $view->render('admin');
