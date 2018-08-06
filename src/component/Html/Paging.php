8fa28f7c8f0bb337927f89a148d17581a5b1ca48
Mikołaj Pich <m.pich@outlook.com>
2018-08-06 12:47:25 +0200

    Add more classes to pagination

--- src/component/Html/Paging.php
+++ src/component/Html/Paging.php
@@ -72,7 +72,9 @@ class Paging
     {
         $this->classes = array_merge([
             'ul' => '',
-            'current' => 'current',
+            'li_current' => '',
+            'a' => '',
+            'a_current' => '',
         ], $classes);
     }
 
@@ -91,6 +93,26 @@ class Paging
         return $this->nesting;
     }
 
+    /**
+     * Put value is not empty.
+     *
+     * @param string       $attribute
+     * @param string|array $value
+     *
+     * @return string
+     */
+    private function isAttribute($attribute, $value)
+    {
+        if (is_array($value)) {
+            array_filter($value);
+            $value = trim(implode(' ', $value));
+
+            return !empty($value) ? ' '.$attribute.'="'.$value.'"' : '';
+        }
+
+        return (isset($value) and !empty($value)) ? ' '.$attribute.'="'.trim($value).'"' : '';
+    }
+
     public function create()
     {
         $nav = $this->getInfo();
@@ -98,57 +120,83 @@ class Paging
         $classes = $this->getClasses();
         $nesting = $this->getNesting();
 
-        $html[] = sprintf('<ul%1$s>', $classes['ul'] ? ' class="'.$classes['ul'].'"' : '');
-        if (!isset($classes['current'])) {
-            $classes['current'] = 'current';
-        }
+        $html[] = sprintf('<ul%1$s>', $this->isAttribute('class', $classes['ul']));
 
         $nest = str_repeat("\t", $nesting);
         $tab = str_repeat("\t", 1 + $nesting);
 
         if ($nav['page'] > 1) {
-            $html[] = sprintf('%1$s<li><a href="%2$s">«</a></li>', $tab, $path.'/page/'.$nav['prev']);
+            $html[] = sprintf(
+                '%1$s<li%2$s><a%3$s href="%4$s">«</a></li>',
+                $tab,
+                $this->isAttribute('class', $classes['li']),
+                $this->isAttribute('class', $classes['a']),
+                $path.'/page/'.$nav['prev']
+            );
         }
         if ($nav['forstart'] > 1) {
-            $html[] = sprintf('%1$s<li><a href="%2$s">1</a></li>', $tab, $path.'/page/1');
+            $html[] = sprintf(
+                '%1$s<li%2$s><a%3$s href="%4$s">1</a></li>',
+                $tab,
+                $this->isAttribute('class', $classes['li']),
+                $this->isAttribute('class', $classes['a']),
+                $path.'/page/1'
+            );
         }
         if ($nav['forstart'] > 2) {
-            $html[] = sprintf('%1$s<li><a>...</a></li>', $tab);
+            $html[] = sprintf(
+                '%1$s<li%2$s><a%3$s>...</a></li>',
+                $tab,
+                $this->isAttribute('class', $classes['li']),
+                $this->isAttribute('class', $classes['a'])
+            );
         }
         for ($nav['forstart']; $nav['forstart'] < $nav['forend']; ++$nav['forstart']) {
             if ((int) $nav['forstart'] === $nav['page']) {
                 $html[] = sprintf(
-                    '%1$s<li class="%2$s"><a href="%3$s">%4$s</a></li>',
+                    '%1$s<li%2$s><a%3$s href="%4$s">%5$s</a></li>',
                     $tab,
-                    $classes['current'],
+                    $this->isAttribute('class', [$classes['li'], $classes['li_current']]),
+                    $this->isAttribute('class', [$classes['a'], $classes['a_current']]),
                     $path.'/page/'.$nav['forstart'],
                     $nav['forstart']
                 );
             }
             if ((int) $nav['forstart'] !== $nav['page']) {
                 $html[] = sprintf(
-                    '%1$s<li><a href="%2$s">%3$s</a></li>',
+                    '%1$s<li%2$s><a%3$s href="%4$s">%5$s</a></li>',
                     $tab,
+                    $this->isAttribute('class', $classes['li']),
+                    $this->isAttribute('class', $classes['a']),
                     $path.'/page/'.$nav['forstart'],
                     $nav['forstart']
                 );
             }
         }
         if ($nav['forstart'] < $nav['allpages']) {
-            $html[] = sprintf('%1$s<li><a>...</a></li>', $tab);
+            $html[] = sprintf(
+                '%1$s<li%2$s><a%3$s>...</a></li>',
+                $tab,
+                $this->isAttribute('class', $classes['li']),
+                $this->isAttribute('class', $classes['a'])
+            );
         }
         if ($nav['forstart'] - 1 < $nav['allpages']) {
             $html[] = sprintf(
-                '%1$s<li><a href="%2$s">%3$s</a></li>',
+                '%1$s<li%2$s><a%3$s href="%4$s">%5$s</a></li>',
                 $tab,
+                $this->isAttribute('class', $classes['li']),
+                $this->isAttribute('class', $classes['a']),
                 $path.'/page/'.$nav['allpages'],
                 $nav['allpages']
             );
         }
         if ($nav['page'] < $nav['allpages']) {
             $html[] = sprintf(
-                '%1$s<li><a href="%2$s">»</a></li>',
+                '%1$s<li%2$s><a%3$s href="%4$s">»</a></li>',
                 $tab,
+                $this->isAttribute('class', $classes['li']),
+                $this->isAttribute('class', $classes['a']),
                 $path.'/page/'.$nav['next']
             );
         }
